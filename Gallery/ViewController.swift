7818389eab32d4d0f4b3c065d036708658e569e9
Bogdan Petkanich <bogdan.petkanich@nerdpeople.com>
2021-03-30 02:49:30 +0300

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by echo on 3/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
